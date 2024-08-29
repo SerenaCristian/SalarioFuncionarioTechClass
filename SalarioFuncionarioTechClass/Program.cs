e5810cc25f265ba2209d5e2e724835d93a50a708
Cristian Serena <devcristian02@gmail.com>
2024-08-28 21:46:11 -0300

    POO salario funcionario

---
+++ SalarioFuncionarioTechClass/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SalarioFuncionarioTechClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario funcionario = new Salario();
+            string sep = new string('.', 50);
+            Console.WriteLine(sep);
+            Console.WriteLine("Programa Calcula Salario Funcionário");
+            Console.WriteLine(sep);
+
+            Console.Write("Digite o nome do Funcionario: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Digite o salario bruto do Funcionario: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine(sep);
+            
+            Console.WriteLine("Dados do funcionário Atualizado: ");
+            Console.WriteLine($"Nome Funcionário: {funcionario.Nome}");
+            Console.WriteLine($"Salario Bruto Funcionário: {funcionario.SalarioBruto}");
+            Console.WriteLine($"Salario liquido do Funcionário: {funcionario.CalculaSalarioLiquido()}");
+        }
+    }
+}
---
+++ SalarioFuncionarioTechClass/Salario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace SalarioFuncionarioTechClass
+{
+    public class Salario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double porcentagem;
+
+
+        public double CalculaSalarioLiquido()
+        {
+            return SalarioBruto - (SalarioBruto * Imposto / 100); ; 
+        }
+
+        public double AumentaSalario()
+        {
+            return SalarioBruto + SalarioBruto * porcentagem / 100.00;
+        }
+    }
+}
