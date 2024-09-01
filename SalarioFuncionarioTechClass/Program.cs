e22cfe94c0d39c2ddc0b5d24f0c0cb199652ab28
Cristian Serena <devcristian02@gmail.com>
2024-09-01 18:31:47 -0300

    Adicionado metodo construtor e criado o metodo ToString para retornar a mensagem do usuario

--- SalarioFuncionarioTechClass/Program.cs
+++ SalarioFuncionarioTechClass/Program.cs
@@ -6,27 +6,30 @@ namespace SalarioFuncionarioTechClass
     {
         static void Main(string[] args)
         {
-            Salario funcionario = new Salario();
+            
             string sep = new string('.', 50);
             Console.WriteLine(sep);
             Console.WriteLine("Programa Calcula Salario Funcionário");
             Console.WriteLine(sep);
 
             Console.Write("Digite o nome do Funcionario: ");
-            funcionario.Nome = Console.ReadLine();
+            string nome = Console.ReadLine();
 
             Console.Write("Digite o salario bruto do Funcionario: ");
-            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+           double salarioBruto = double.Parse(Console.ReadLine());
 
             Console.Write("Digite o valor do imposto: ");
-            funcionario.Imposto = double.Parse(Console.ReadLine());
-            
+            double imposto = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor do Aumento: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+
             Console.WriteLine(sep);
-            
-            Console.WriteLine("Dados do funcionário Atualizado: ");
-            Console.WriteLine($"Nome Funcionário: {funcionario.Nome}");
-            Console.WriteLine($"Salario Bruto Funcionário: {funcionario.SalarioBruto}");
-            Console.WriteLine($"Salario liquido do Funcionário: {funcionario.CalculaSalarioLiquido()}");
+
+            Salario funcionario = new Salario(nome, salarioBruto, imposto, porcentagem);
+
+            Console.WriteLine(funcionario);
+
         }
     }
 }
--- SalarioFuncionarioTechClass/Salario.cs
+++ SalarioFuncionarioTechClass/Salario.cs
@@ -8,17 +8,33 @@ namespace SalarioFuncionarioTechClass
         public string Nome;
         public double SalarioBruto;
         public double Imposto;
-        public double porcentagem;
+        public double Porcentagem;
 
+        public Salario(string nome, double salarioBruto, double imposto, double porcentagem)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+            Porcentagem = porcentagem;
+        }
 
         public double CalculaSalarioLiquido()
         {
             return SalarioBruto - (SalarioBruto * Imposto / 100); ; 
         }
 
-        public double AumentaSalario()
+        public double AumentaSalario(double porcentagem)
         {
             return SalarioBruto + SalarioBruto * porcentagem / 100.00;
         }
+
+        public override string ToString()
+        {
+            return $"Nome do Funcionário: {Nome}\n" +
+                   $"Salário Bruto: {SalarioBruto:F2}\n" +
+                   $"Imposto: {Imposto:F2}%\n" +
+                   $"Salário Líquido: {CalculaSalarioLiquido():F2}";
+        }
+
     }
 }
